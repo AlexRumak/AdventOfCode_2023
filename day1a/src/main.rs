6141289431b94fa854103b8041c61ad3d480515d
Alex Rumak <alexanderrumak@gmail.com>
2023-12-14 23:21:36 -0500

    Updated solution 1 to use find.

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -13,24 +13,11 @@ fn main() {
 fn sum_digits(contents: String) -> i32 {
     let mut sum = 0;
     for line in contents.lines() {
-        let forward_iter = line.chars();
-        let reverse_iter = line.chars().rev();
+        let mut forward_iter = line.chars();
+        let mut reverse_iter = line.chars().rev();
         
-        let mut first_char: Option<char> = None;
-        let mut sec_char: Option<char> = None;
-        for el in forward_iter {
-            if el.is_digit(10) {
-                first_char = Some(el);
-                break;
-            }
-        }
-
-        for el in reverse_iter {
-            if el.is_digit(10) {
-                sec_char = Some(el);
-                break;
-            }
-        }
+        let first_char = forward_iter.find(|&c| c.is_ascii_digit());
+        let sec_char = reverse_iter.find(|&c| c.is_ascii_digit());
         
         let digits = format!("{}{}", first_char.unwrap(), sec_char.unwrap());
         sum += digits.parse::<i32>().unwrap();
