1005ebb0eb49b3efa32924849ad6bad1f305651d
Alex Rumak <alexanderrumak@gmail.com>
2023-12-29 12:01:30 -0500

    Day 8b in rust, day 9a start.

--- day8b/src/main.rs
+++ day8b/src/main.rs
@@ -1,6 +1,5 @@
-use petgraph::{graph::{NodeIndex, Graph}, data::Build, visit::NodeRef};
+use petgraph::{graph::{NodeIndex, Graph}, visit::EdgeRef};
 use std::{fs::{self, File}, collections::HashMap, io::Write};
-use std::io::prelude::*;
 
 fn main() {
     let contents = fs::read_to_string("input.txt")
@@ -10,27 +9,122 @@ fn main() {
     let instructions = lines.next().expect("Instructions should exist");
     lines.next(); // skip blank line
 
-    let mut g: Graph<String, u32> = Graph::new();
+    let mut g: Graph<String, String> = Graph::new();
     
+    let mut map: HashMap<String, NodeIndex> = HashMap::new();
+
     for line in lines {
         let mut split = line.split("=");
         let node_val = split.next().expect("Should have a left side").trim().to_string();
         let edges_to = split.next().expect("Should have a right side").trim().to_string();
-        let edges_split = edges_to.split(", ");
+        let mut edges_split = edges_to.split(", ");
 
-        let a = g.add_node(node_val); 
+        let a = get_or_add_node(&mut g, &mut map, node_val); 
 
-        for edge in edges_split {
-            let edge_val = edge.trim().replace('(', "").replace(')', "");
-            let b = g.add_node(edge_val);
-            g.add_edge(a, b, 1);
-        }
+        let left_val = edges_split.next().expect("Should have a value").trim().replace('(', "");
+        let right_val = edges_split.next().expect("Should have a value").trim().replace(')', "");
+
+        let left = get_or_add_node(&mut g, &mut map, left_val); 
+        let right = get_or_add_node(&mut g, &mut map, right_val); 
+        g.add_edge(a, left, String::from("L"));
+        g.add_edge(a, right, String::from("R"));
+
     }
 
     write_to_file_graph(&g).expect("Should have been able to write to file");
+
+    let starting_points = get_starting_points(&g, &|x| (g.node_weight(x).expect("Should have a weight").contains('A')));
+
+    let answers : Vec<u64> = starting_points.iter()
+        .map(|x: &NodeIndex| get_cycle_length(&g, *x, &instructions, 100000) as u64)
+        .collect();
+
+    println!("Answer: {}", calculate_lcm(answers));
 }
 
-fn write_to_file_graph(g: &Graph<String, u32>) -> std::io::Result<()> {
+fn calculate_lcm(values: Vec<u64>) -> u64 {
+    let mut lcm = values[0];
+    for i in 1..values.len() {
+        lcm = (lcm * values[i]) / gcd(lcm, values[i]);
+    }
+    lcm
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+fn get_or_add_node(g: &mut Graph<String, String>, map: &mut HashMap<String, NodeIndex>, val: String) -> NodeIndex {
+    if map.contains_key(&val) {
+        return *map.get(&val).expect("Should have a value");
+    }
+
+    let node = g.add_node(val.clone());
+    map.insert(val, node);
+    node
+}
+
+fn get_starting_points(g: &Graph<String, String>, f: &dyn Fn(NodeIndex) -> bool) -> Vec<NodeIndex> {
+    let mut starting_points = Vec::new();
+    for node in g.node_indices() {
+        if f(node) {
+            starting_points.push(node);
+        }
+    }
+    starting_points
+} 
+
+fn get_cycle_length(g: &Graph<String, String>, starting_point: NodeIndex, instructions: &str, max: usize) -> u32 {
+
+    let mut current_node = starting_point;
+    let instructions: Vec<char> = instructions.chars().collect();
+    let mut index: usize = 0;
+    
+    let mut answers : Vec<u32> = Vec::new();
+    while index < max {
+        let node_val = g.node_weight(current_node).expect("Should have a weight");
+        if node_val.contains('Z') {
+            let steps: usize = index + 1;
+            println!("Found Z for starting point {} at step {}", g.node_weight(starting_point).expect("Should have a weight"), steps);
+            answers.push(steps as u32);
+        }
+
+        let edges = g.edges(current_node);
+        let instruction = instructions[index % instructions.len()];
+        for edge in edges {
+            let weight = edge.weight();
+            if weight.contains(instruction) {
+                current_node = edge.target();
+                break;
+            }
+        }
+
+        index += 1;
+    }
+    
+    if answers.len() < 3 {
+        panic!("Not enough answers to calculate differences, please increase max.")
+    }
+
+    let differences = answers.windows(2).map(|x| x[1] - x[0]).collect::<Vec<u32>>();
+    let all_equal = differences.windows(2).all(|x| x[0] == x[1]);
+
+    if !all_equal {
+        panic!("Not all differences are equal - cannot use LCM algorithm")
+    }
+
+    return differences[0];
+}
+
+fn write_to_file_graph(g: &Graph<String, String>) -> std::io::Result<()> {
     let mut file = File::create("output.txt")?;
     writeln!(file, "digraph G {{")?;
     for edge in g.edge_indices() {
